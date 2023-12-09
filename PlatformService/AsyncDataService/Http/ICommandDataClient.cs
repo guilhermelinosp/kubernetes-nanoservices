313b983f397337846e570a34ebf0b7ff8d73af28
Guilherme Lino <guilhermelinosp@gmail.com>
2023-12-08 21:29:05 -0300

    feat: init project

--- PlatformService/AsyncDataService/Http/ICommandDataClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using PlatformService.Dtos;
-
-namespace PlatformService.AsyncDataService.Http
-{
-	public interface ICommandDataClient
-	{
-		Task SendPlatformToCommand(PlatformReadDto platform);
-	}
-}
